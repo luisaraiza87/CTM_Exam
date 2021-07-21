0336d448347f97707e3b93dc31ece4d8b93e52fd
Luis Araiza <laraiza@insala.com>
2021-07-21 17:49:06 -0500

    First Approach;

---
+++ CTMExam/Controllers/ExamController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTMExam.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public string Get(int N)
+        {
+            List<string> result = FizzBuzz(N);            
+
+            return string.Join(',', result);
+        }
+
+        public List<string> FizzBuzz(int n)
+        {
+
+            List<string> list = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    list.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    list.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    list.Add("Buzz");
+                }
+                else
+                {
+                    list.Add(i.ToString());
+                }
+            }
+
+            return list;
+
+        }
+    }
+}
